65f023eab0d3919f9f736e843e8062dfe79c38c3
Adevanes <celebratte@gmail.com>
2021-09-13 20:58:56 -0300

    meu segundo commit

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -2,27 +2,35 @@
 
 context('Funcionalidade Login', () =>{
 
-    it('Deve fazer o login com sucesso', () =>{
+    beforeEach(() => {
         cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/') //visitou uma url para testar
-        cy.get('#username').type('aluno_ebac@teste.com')
-        cy.get('#password').type('teste@teste.com')
-        cy.get('.woocommerce-form > .button').click()
+    });
+    
+   afterEach(() => {
+       cy.screenshot()
+   });
 
-        cy.get('.page-title').should('contain', 'Minha conta')
+    it('Deve fazer o login com sucesso', () =>{
+       
+        cy.get('#username').type('aluno_ebac@teste.com') // digitou um login válido
+        cy.get('#password').type('teste@teste.com') // digitou uma senha válida
+        cy.get('.woocommerce-form > .button').click() // clicou no botao de acesso
+
+        cy.get('.page-title').should('contain', 'Minha conta') // acessou a pagina do usuario
         cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, aluno_ebac ')
     })
     
     it('Deve exibir uma mensagem de erro ao inserir usuario inválido', () =>{
-        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/') //visitou uma url para testar
-        cy.get('#username').type('ebac@teste.com')
-        cy.get('#password').type('teste@teste')
-        cy.get('.woocommerce-form > .button').click()
+       
+        cy.get('#username').type('ebac@teste.com') //digitou um login inválido
+        cy.get('#password').type('teste@teste') // digitou uma senha inválida
+        cy.get('.woocommerce-form > .button').click() // clicou no  botao de acesso
 
         cy.get('.woocommerce-error > li').should('contain', 'Endereço de e-mail desconhecido')
     })
 
     it('Deve exibir uma mensagem de erro ao inserir senha inválida', () =>{
-        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/') //visitou uma url para testar
+       
         cy.get('#username').type('ebac@teste.com')
         cy.get('#password').type('teste@teste')
         cy.get('.woocommerce-form > .button').click()
